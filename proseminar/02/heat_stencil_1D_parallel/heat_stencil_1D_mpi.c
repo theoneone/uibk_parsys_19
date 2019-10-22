895df63706a7d4e1c99464dee91f57f9088544eb
theoneone <theone-mail@a1.net>
2019-10-22 03:37:57 +0200

    parallel version complete - doesn't work right now

--- proseminar/02/heat_stencil_1D_parallel/heat_stencil_1D_mpi.c
+++ proseminar/02/heat_stencil_1D_parallel/heat_stencil_1D_mpi.c
@@ -1,6 +1,7 @@
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 
 typedef double value_t;
 
@@ -15,6 +16,12 @@ Vector createVector(int N);
 void releaseVector(Vector m);
 
 void printTemperature(Vector m, int N);
+
+void print_rank0(int rank, const char *format, ...);
+
+int calculate_r0(int N, int T);
+
+void calculate_part(int size, int rank, int N, int T);
 
 // -- simulation code ---
 
@@ -31,87 +38,12 @@ int main(int argc, char **argv) {
 		N = atoi(argv[1]);
 	}
 	int T = N * 500;
-	printf("Computing heat-distribution for room size N=%d for T=%d timesteps\n", N, T);
-
-	// ---------- setup ----------
-
-	// create a buffer for storing temperature fields
-	Vector A = createVector(N);
-
-	// set up initial conditions in A
-	for (int i = 0; i < N; i++) {
-		A[i] = 273; // temperature is 0° C everywhere (273 K)
-	}
-
-	// and there is a heat source in one corner
-	int source_x = N / 4;
-	A[source_x] = 273 + 60;
-
-	printf("Initial:\t");
-	printTemperature(A, N);
-	printf("\n");
-
-	// ---------- compute ----------
-
-	// create a second buffer for the computation
-	Vector B = createVector(N);
-
-	// for each time step ..
-	for (int t = 0; t < T; t++) {
-		// .. we propagate the temperature
-		for (long long i = 0; i < N; i++) {
-			// center stays constant (the heat is still on)
-			if (i == source_x) {
-				B[i] = A[i];
-				continue;
-			}
-
-			// get temperature at current position
-			value_t tc = A[i];
-
-			// get temperatures of adjacent cells
-			value_t tl = (i != 0) ? A[i - 1] : tc;
-			value_t tr = (i != N - 1) ? A[i + 1] : tc;
-
-			// compute new temperature at current position
-			B[i] = tc + 0.2 * (tl + tr + (-2 * tc));
-		}
-
-		// swap matrices (just pointers, not content)
-		Vector H = A;
-		A = B;
-		B = H;
-
-		// show intermediate step
-		if (!(t % 1000)) {
-			printf("Step t=%d:\t", t);
-			printTemperature(A, N);
-			printf("\n");
-		}
-	}
-
-	releaseVector(B);
-
-	// ---------- check ----------
-
-	printf("Final:\t\t");
-	printTemperature(A, N);
-	printf("\n");
 
 	int success = 1;
-	for (long long i = 0; i < N; i++) {
-		value_t temp = A[i];
-		if (273 <= temp && temp <= 273 + 60)
-			continue;
-		success = 0;
-		break;
+	if (rank == 0) {
+		printf("Computing heat-distribution for room size N=%d for T=%d timesteps\n", N, T);
 	}
-
-	printf("Verification: %s\n", (success) ? "OK" : "FAILED");
-
-	// ---------- cleanup ----------
-
-	releaseVector(A);
+	calculate_part(size, rank, N, T);
 
 	MPI_Finalize();
 	// done
@@ -160,4 +92,200 @@ void printTemperature(Vector m, int N) {
 	}
 	// right wall
 	printf("X");
+}
+
+/**
+ * print message like printf, but only if rank == 0
+ *
+ * @param format a format string
+ * @param ... parameters defined by the format string, the number of parameters must be equal to format specifiers in %c format
+ */
+void print_rank0(int rank, const char *format, ...) {
+	if (rank == 0) {
+		char prev = format[0];
+		int n = prev == '%' ? 1 : 0;
+		for(int i = 1; format[i] != '\0'; ++i) {
+			if(format[i] == '%' && prev != '%') {
+				++n;
+			} else if(format[i] == '%') {
+				--n;
+			}
+			prev = format[i];
+		}
+		va_list valist;
+		va_start(valist, format);
+		vprintf(format, valist);
+		va_end(valist);
+
+	}
+}
+
+int calculate_r0(int N, int T) {
+	// ---------- setup ----------
+	// create a buffer for storing temperature fields
+	Vector A = createVector(N);
+	// set up initial conditions in A
+	for (int i = 0; i < N; i++) {
+		A[i] = 273; // temperature is 0° C everywhere (273 K)
+	}
+	// and there is a heat source in one corner
+	int source_x = N / 4;
+	A[source_x] = 273 + 60;
+	printf("Initial:\t");
+	printTemperature(A, N);
+	printf("\n");
+	// ---------- compute ----------
+	// create a second buffer for the computation
+	Vector B = createVector(N);
+	// for each time step ..
+	for (int t = 0; t < T; t++) {
+		// .. we propagate the temperature
+		for (long long i = 0; i < N; i++) {
+			// center stays constant (the heat is still on)
+			if (i == source_x) {
+				B[i] = A[i];
+				continue;
+			}
+			// get temperature at current position
+			value_t tc = A[i];
+			// get temperatures of adjacent cells
+			value_t tl = (i != 0) ? A[i - 1] : tc;
+			value_t tr = (i != N - 1) ? A[i + 1] : tc;
+			// compute new temperature at current position
+			B[i] = tc + 0.2 * (tl + tr + (-2 * tc));
+		}
+		// swap matrices (just pointers, not content)
+		Vector H = A;
+		A = B;
+		B = H;
+		// show intermediate step
+		// modification: keep number of lines to print stable
+		if (!(t % N)) {
+			printf("Step t=%d:\t", t);
+			printTemperature(A, N);
+			printf("\n");
+		}
+	}
+	releaseVector(B);
+	// ---------- check ----------
+	printf("Final:\t\t");
+	printTemperature(A, N);
+	printf("\n");
+	int success = 1;
+	for (long long i = 0; i < N; i++) {
+		value_t temp = A[i];
+		if (273 <= temp && temp <= 273 + 60)
+			continue;
+
+		success = 0;
+		break;
+	}
+	printf("Verification: %s\n", (success) ? "OK" : "FAILED");
+	// ---------- cleanup ----------
+	releaseVector(A);
+	return success;
+}
+
+void calculate_part(int size, int rank, int N_elements, int T) {
+	// ---------- setup ----------
+	int N = (N_elements + size-1) / size;
+	// create a buffer A for storing temperature fields
+	// create a second buffer B for the computation
+	Vector A, B;
+	if (rank == 0) {
+		A = createVector(N_elements);
+		B = createVector(N_elements);
+	} else {
+		A = createVector(N);
+		B = createVector(N);
+	}
+	// set up initial conditions in A
+	for (int i = 0; i < N; i++) {
+		A[i] = 273; // temperature is 0° C everywhere (273 K)
+	}
+	// and there is a heat source in one corner
+	int source_x = -1; // negative value: source not in this rank
+	if (N * rank < N_elements / 4 && N_elements / 4 < (N+1) * rank) {
+		source_x = (N_elements / 4) % N;
+		A[source_x] = 273 + 60;
+	}
+	if (rank == 0) {
+		printf("Initial:\t");
+		printTemperature(A, N_elements);
+		printf("\n");
+	}
+	// ---------- compute ----------
+	// for each time step ..
+	for (int t = 0; t < T; t++) {
+		if (rank != 0) {
+			MPI_Send(&(A[0]), 1, MPI_INT, rank-1, 42, MPI_COMM_WORLD);
+		}
+		if (rank != size-1) {
+			MPI_Send(&(A[N-1]), 1, MPI_INT, rank+1, 42, MPI_COMM_WORLD);
+		}
+		// .. we propagate the temperature
+		for (long long i = 0; i < N; i++) {
+			// center stays constant (the heat is still on)
+			if (i == source_x && source_x > 0) {
+				B[i] = A[i];
+				continue;
+			}
+			// get temperature at current position
+			value_t tc = A[i];
+			// get temperatures of adjacent cells
+			value_t tl;
+			value_t tr;
+			if (i != 0) {
+				tl = A[i-1];
+			} else if (i == 0 && rank != 0) {
+				MPI_Recv(&tl, 1, MPI_INT, rank-1, 42, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			} else {
+				tl = tc;
+			}
+			if (i != N-1) {
+				tr = A[i+1];
+			} else if (i == N-1 && rank != size-1) {
+				MPI_Recv(&tr, 1, MPI_INT, rank+1, 42, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			} else {
+				tr = tc;
+			}
+			// compute new temperature at current position
+			B[i] = tc + 0.2 * (tl + tr + (-2 * tc));
+		}
+		// swap matrices (just pointers, not content)
+		Vector H = A;
+		A = B;
+		B = H;
+		// show intermediate step
+		// modification: keep number of lines to print stable
+		if (!(t % N) && rank == 0) {
+			for (int r = 1; r < size; ++r) {
+				MPI_Recv(A+r*size, N, MPI_INT, rank, 21, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			}
+			printf("Step t=%d:\t", t);
+			printTemperature(A, N);
+			printf("\n");
+		} else if (!(t % N)) {
+			MPI_Send(A, N, MPI_INT, 0, 21, MPI_COMM_WORLD);
+		}
+	}
+	releaseVector(B);
+	// ---------- check ----------
+	int success = 1;
+	if (rank == 0) {
+		printf("Final:\t\t");
+		printTemperature(A, N);
+		printf("\n");
+		for (long long i = 0; i < N; i++) {
+			value_t temp = A[i];
+			if (273 <= temp && temp <= 273 + 60)
+				continue;
+
+			success = 0;
+			break;
+		}
+	}
+	print_rank0(rank, "Verification: %s\n", (success) ? "OK" : "FAILED");
+	// ---------- cleanup ----------
+	releaseVector(A);
 }
